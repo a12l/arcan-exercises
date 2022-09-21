97ad02d04033da0f5642f12460ffde07216d5d8c
Albin Otterhäll <dev@a12l.xyz>
2022-09-21 23:18:49 +0200

    Add solution to Exercise 1: Loading and Displaying an image
    Task
    ====
    
    	Create a Program that displays a full-screen, single color surface, call it exercise1.
    
    	*Hint 1:* Arcan enforces a naming convention, the main folder should have the same name as the desired application name, and there should be at least one file with the same name as the folder (+ the extension .lua). That file should have at least one function (the entrypoint), again named the same as the application name.
    
    	*Hint 2:* All built-in functions have a corresponding .lua file in the doc/ subdirectory, where you can find arguments, description and example of use. There is also a script (mangen) that can generate man-pages from these scripts.
    
    	*Hint 3*: Look for a function called color_surface, note that newly created surfaces start out hidden (use show_image, blend_image), also check the contents of the globals VRESW and VRESH.
    
    `Exercise 1: Loading and Displaying an image<https://github.com/letoram/arcan/wiki/exercise_1#2-loading-and-displaying-an-image>`_

--- exercise1/exercise1.lua
+++ exercise1/exercise1.lua
@@ -1,4 +1,22 @@
-function exercise1()
-	local a = color_surface(1920, 1080, 50, 93, 181);
-	show_image(a);
+local argv_cmds = {};
+
+function exercise1(argv)
+	for k, v in ipairs(argv) do
+		if argv_cmds[v] then
+			argv_cmds[v](argv, k);
+		end
+	end
+end
+
+argv_cmds["bg"] = function(argv, index)
+	local img_path = argv[index + 1];
+	local _, type = resource(img_path, APPL_RESOURCE);
+
+	if (type == "file") then
+		local img = load_image(img_path);
+		show_image(img);
+	else
+		warning("Given image does not exist.");
+		return shutdown();
+	end
 end
